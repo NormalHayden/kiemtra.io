a0b0614749d12c937082fdbefe6ca976edb97545
Ktore23 <165562567+Ktore23@users.noreply.github.com>
2025-03-13 17:13:55 +0700

    Delete kiemtra.io directory

--- kiemtra.io/huy_9048/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let btn = document.querySelector(".btn-primary");
-
-    btn.addEventListener("click", function() {
-        alert("Cảm ơn bạn đã ghé thăm trang cá nhân của tôi!");
-    });
-});
